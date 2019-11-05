26a7ca70eed6d734e56fda95c118af6b1470f6b0
JamesB <git@jamesberry.co.uk>
2019-11-05 16:59:19 +0000

    Added base "Enemy" class script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string enemyName;
+    public Mesh model;
+
+    public int health;
+
+    public int damage;
+
+    public void TakeDamage(int hitDamage)
+    {
+        health -= hitDamage;
+    }
+
+    public void Die()
+    {
+        Debug.Log(enemyName + " has died");
+        Object.Destroy(this);
+    }
+}
