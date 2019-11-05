8ada646c18ee064ecd1dca59da8399f90e1690cd
HDonovan96 <harry.bd11@gmail.com>
2019-11-05 16:57:02 +0000

    Added variables to track quick time events state and the response

--- Assets/Scripts/QTFramework.cs
+++ Assets/Scripts/QTFramework.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Quick time event handler.
+// Code from https://gist.github.com/grimmdev/b85994d1b7cad444eb69 has been used as inspiration.
+
 public class QTFramework : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum CurrentState 
     {
-        
-    }
+        Ready,
+        Active,
+        Completed
+    };
 
-    // Update is called once per frame
-    void Update()
+    private CurrentState QTState = CurrentState.Ready;
+
+    private enum Response
     {
-        
-    }
+        Null,
+        Success,
+        Fail
+    };
+
+    private Response QTResponse = Response.Null;
 }
