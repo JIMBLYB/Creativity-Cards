ed409dc266825a8f016d65b24af3fe91e69bddad
James Berry <git@jamesberry.co.uk>
2019-11-08 13:41:06 +0000

    Added basic Animal.cs, only covers pricing and initialises some variables

---
+++ Assets/Scripts/Classes/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    // GameObject references
+    public GameController gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+
+    // Variables for use during battle
+    public int level;
+    public int damage;
+    public float levelUpIncrease;
+    public float hitChance;
+    public float cooldownDuration;
+
+    // Variables for shop
+    public bool unlocked;
+    public int buyPrice;
+    public int currentPrice;
+    public float increaseMult;
+
+    public void Pricing()
+    {
+        if (!unlocked)
+        {
+            currentPrice = buyPrice;
+        }
+        else
+        {
+            currentPrice = Mathf.CeilToInt(currentPrice * increaseMult);
+        }
+    }
+
+    public void Purchasing()
+    {
+        if (gameController.money >= currentPrice)
+        {
+            if (!unlocked)
+            {
+                unlocked = true;
+                level = 1;
+                Pricing();
+            }
+            else
+            {
+                level++;
+                Pricing();
+                damage = Mathf.FloorToInt(damage * levelUpIncrease);
+            }
+        }
+    }
+}
