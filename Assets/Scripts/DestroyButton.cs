ca8090a780a38856fc577b4e62cb37f6c08bf2b4
HDonovan96 <harry.bd11@gmail.com>
2019-11-15 11:35:55 +0000

    BoxCollider2D component is now included in the 'QTButton' prefab rather than being added by 'QTFramework' script

--- Assets/Scripts/DestroyButton.cs
+++ Assets/Scripts/DestroyButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyButton : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("clash");
         Destroy(gameObject);
