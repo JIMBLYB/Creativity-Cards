aa9f50204fda268c458505ae570fcfae069e0c76
HDonovan96 <harry.bd11@gmail.com>
2019-11-15 11:30:31 +0000

    Added code to destoy buttons to 'DestroyButton' script

--- Assets/Scripts/DestroyButton.cs
+++ Assets/Scripts/DestroyButton.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class DestroyButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("clash");
+        Destroy(gameObject);
+        Debug.Log("Kaboom");
     }
 }
