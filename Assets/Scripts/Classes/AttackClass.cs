5f943215392277816143a53e81990a2e79786a80
HDonovan96 <harry.bd11@gmail.com>
2019-11-16 14:42:47 +0000

    Implemented code to begin a quick time event into 'AttackClass' script. Deleted 'TestScript' script

--- Assets/Scripts/Classes/AttackClass.cs
+++ Assets/Scripts/Classes/AttackClass.cs
@@ -7,5 +7,23 @@ public class AttackClass : MonoBehaviour
     public string attack_name;
     public int cooldown, ID, damage;
     public float accuracy;
-    public List<KeyCode> attackSequence;
+    public List<KeyCode> attackSequence = new List<KeyCode>();
+    public Object QTAreaPrefab;
+
+    // Placeholder for testing purposes
+    void Start()
+    {
+        attackSequence.Add(KeyCode.A);
+        attackSequence.Add(KeyCode.B);
+        attackSequence.Add(KeyCode.C);
+    }
+
+    // Instantiates and begins the quick time event
+    public void OnButtonPress()
+    {
+        GameObject QTArea = Instantiate(QTAreaPrefab, transform.parent) as GameObject;
+
+        QTFramework QTFramework = QTArea.GetComponentInChildren<QTFramework>();
+        QTFramework.sequence = attackSequence;
+    }
 }
