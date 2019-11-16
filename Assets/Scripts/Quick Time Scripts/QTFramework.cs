417250c917467d3d99c4a2bbd61c39f32ed496dd
HDonovan96 <harry.bd11@gmail.com>
2019-11-16 13:04:31 +0000

    Removed unsused code from 'QTFramework' script

--- Assets/Scripts/Quick Time Scripts/QTFramework.cs
+++ Assets/Scripts/Quick Time Scripts/QTFramework.cs
@@ -12,24 +12,6 @@ using TMPro;
 
 public class QTFramework : MonoBehaviour
 {
-    private enum CurrentState 
-    {
-        Ready,
-        Active,
-        Completed
-    };
-
-    private CurrentState QTState = CurrentState.Ready;
-
-    private enum Response
-    {
-        Null,
-        Success,
-        Fail
-    };
-
-    private Response QTResponse = Response.Null;
-
     // Stores the keys in the quick time event sequence.
     // This should be passed to the framework
     public List<KeyCode> sequence;
