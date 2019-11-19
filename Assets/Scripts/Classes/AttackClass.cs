dfe0ca44cf4a4173404cffefb9c6c175fec190ef
HDonovan96 <harry.bd11@gmail.com>
2019-11-19 13:28:33 +0000

    Commented out the attack sequneces in 'AttackClass' script as they were causing errors

--- Assets/Scripts/Classes/AttackClass.cs
+++ Assets/Scripts/Classes/AttackClass.cs
@@ -27,48 +27,48 @@ public class AttackClass : MonoBehaviour
         QTFramework.sequence = attackSequence;
     }
     
-    public void LightAttack()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.L);
-        quickTimeKeys.Add(KeyCode.I);
-        quickTimeKeys.Add(KeyCode.G);
-        quickTimeKeys.Add(KeyCode.H);
-        quickTimeKeys.Add(KeyCode.T);
-        StartQuickTime();
-    }
+    // public void LightAttack()
+    // {
+    //     quickTimeKeys.Clear();
+    //     quickTimeKeys.Add(KeyCode.L);
+    //     quickTimeKeys.Add(KeyCode.I);
+    //     quickTimeKeys.Add(KeyCode.G);
+    //     quickTimeKeys.Add(KeyCode.H);
+    //     quickTimeKeys.Add(KeyCode.T);
+    //     StartQuickTime();
+    // }
 
-    public void MediumAttack()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.M);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.D);
-        quickTimeKeys.Add(KeyCode.I);
-        quickTimeKeys.Add(KeyCode.U);
-        quickTimeKeys.Add(KeyCode.M);
-        StartQuickTime();
-    }
+    // public void MediumAttack()
+    // {
+    //     quickTimeKeys.Clear();
+    //     quickTimeKeys.Add(KeyCode.M);
+    //     quickTimeKeys.Add(KeyCode.E);
+    //     quickTimeKeys.Add(KeyCode.D);
+    //     quickTimeKeys.Add(KeyCode.I);
+    //     quickTimeKeys.Add(KeyCode.U);
+    //     quickTimeKeys.Add(KeyCode.M);
+    //     StartQuickTime();
+    // }
 
-    public void HeavyAttack()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.H);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.A);
-        quickTimeKeys.Add(KeyCode.V);
-        quickTimeKeys.Add(KeyCode.Y);
-        StartQuickTime();
-    }
+    // public void HeavyAttack()
+    // {
+    //     quickTimeKeys.Clear();
+    //     quickTimeKeys.Add(KeyCode.H);
+    //     quickTimeKeys.Add(KeyCode.E);
+    //     quickTimeKeys.Add(KeyCode.A);
+    //     quickTimeKeys.Add(KeyCode.V);
+    //     quickTimeKeys.Add(KeyCode.Y);
+    //     StartQuickTime();
+    // }
 
-    public void Sheep()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.S);
-        quickTimeKeys.Add(KeyCode.H);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.P);
-        StartQuickTime();
-    }
+    // public void Sheep()
+    // {
+    //     quickTimeKeys.Clear();
+    //     quickTimeKeys.Add(KeyCode.S);
+    //     quickTimeKeys.Add(KeyCode.H);
+    //     quickTimeKeys.Add(KeyCode.E);
+    //     quickTimeKeys.Add(KeyCode.E);
+    //     quickTimeKeys.Add(KeyCode.P);
+    //     StartQuickTime();
+    // }
 }
