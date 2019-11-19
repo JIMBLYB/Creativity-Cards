204cee55d9f8a1e415a161620ac5ead3a63eb828
HDonovan96 <harry.bd11@gmail.com>
2019-11-19 13:26:32 +0000

    Begin 'TeamSelection' script

---
+++ Assets/Scripts/UI scripts/TeamSelection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using TMPro;
+
+public class TeamSelection : MonoBehaviour
+{
+    // Script is designed to sit in 'Canvas - TeamSelect' in the 'TeamSelect' scene
+
+    public void OnAttackClick(Button button)
+    {
+        Transform teamSlots = transform.Find("Team Slots");
+
+        TMP_Text[] selectedAttacks = teamSlots.GetComponentsInChildren<TMP_Text>();
+
+        Debug.Log(selectedAttacks[0]);
+    }
+}
