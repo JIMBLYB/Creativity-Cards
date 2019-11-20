d480433b757a3ed5cd8cb50d650a6df7fad20ca8
HDonovan96 <harry.bd11@gmail.com>
2019-11-20 14:07:06 +0000

    'BattleMenuEvent' script now correctly activates quick time events based on the button pressed

--- Assets/Scripts/UI scripts/BattleMenuEvent.cs
+++ Assets/Scripts/UI scripts/BattleMenuEvent.cs
@@ -1,61 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleMenuEvent : MonoBehaviour
 {
-    private List<KeyCode> quickTimeKeys = new List<KeyCode>();
+    public List<KeyCode> quickTimeKeys;
     public Object areaPrefab;
-    private void StartQuickTime()
-    {
-        GameObject QTArea = Instantiate(areaPrefab, transform.parent) as GameObject;
 
-        QTFramework QTFramework = QTArea.GetComponent<QTFramework>();
+    public void OnAttackSelect(Button button)
+    {
+        // Instantiates the area that the quick time event will occur in as a child of the canvas.
+        GameObject QTArea = Instantiate(areaPrefab, transform) as GameObject;
+
+        QTFramework QTFramework = QTArea.GetComponentInChildren<QTFramework>();
+
+        // Checks which attack was selected and then sets the quick time button sequence accordingly.
+        string selectedAttack = button.GetComponentInChildren<Text>().text;
+        switch (selectedAttack)
+        {
+            case "Light":
+            {
+                quickTimeKeys = new List<KeyCode>
+                {
+                    KeyCode.L, 
+                    KeyCode.I, 
+                    KeyCode.G, 
+                    KeyCode.H, 
+                    KeyCode.T
+                };
+                break;
+            }
+            case "Medium":
+            {
+                quickTimeKeys = new List<KeyCode>
+                {
+                    KeyCode.M, 
+                    KeyCode.E, 
+                    KeyCode.D, 
+                    KeyCode.I, 
+                    KeyCode.U
+                };
+                break;
+            }
+            case "Heavy":
+            {
+                quickTimeKeys = new List<KeyCode>
+                {
+                    KeyCode.H, 
+                    KeyCode.E, 
+                    KeyCode.A, 
+                    KeyCode.V, 
+                    KeyCode.Y
+                };
+                break;
+            }
+            case "Sheep":
+            {
+                quickTimeKeys = new List<KeyCode>
+                {
+                    KeyCode.S, 
+                    KeyCode.H, 
+                    KeyCode.E, 
+                    KeyCode.E, 
+                    KeyCode.P
+                };
+                break;
+            }
+            default:
+                Debug.Log("ERROR: Unkown attack");
+                break;
+        }
+
         QTFramework.sequence = quickTimeKeys;
-    }
-
-    public void LightAttack()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.L);
-        quickTimeKeys.Add(KeyCode.I);
-        quickTimeKeys.Add(KeyCode.G);
-        quickTimeKeys.Add(KeyCode.H);
-        quickTimeKeys.Add(KeyCode.T);
-        StartQuickTime();
-    }
-
-    public void MediumAttack()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.M);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.D);
-        quickTimeKeys.Add(KeyCode.I);
-        quickTimeKeys.Add(KeyCode.U);
-        quickTimeKeys.Add(KeyCode.M);
-        StartQuickTime();
-    }
-
-    public void HeavyAttack()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.H);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.A);
-        quickTimeKeys.Add(KeyCode.V);
-        quickTimeKeys.Add(KeyCode.Y);
-        StartQuickTime();
-    }
-
-    public void Sheep()
-    {
-        quickTimeKeys.Clear();
-        quickTimeKeys.Add(KeyCode.S);
-        quickTimeKeys.Add(KeyCode.H);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.E);
-        quickTimeKeys.Add(KeyCode.P);
-        StartQuickTime();
     }
 }
