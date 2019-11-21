e4189fbce00f7a3258f6f199a5b8d75f76f1bdfd
Alex Williamson <alex_williamson@rocketmail.com>
2019-11-21 17:22:39 +0000

    Fixed a bug where multiple attacks could be triggered caused by redundant changes to the PlayerAttack script

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -10,7 +10,7 @@ public class PlayerAttack : MonoBehaviour
     public string attackSelected = string.Empty;
     private bool going = false;
     public bool returning = false;
-    private object selected_attack;
+    private bool attacking;
     private GameObject rootUI;
     public Object areaPrefab;
     QTFramework QTFramework;
@@ -95,68 +95,74 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         //checks if an attack button has been pressed
-        if (!string.IsNullOrEmpty(attackSelected))
+        if (!attacking)
         {
-            //checks if you click on an enemy
-            if (Input.GetMouseButtonDown(0))
+            if (!string.IsNullOrEmpty(attackSelected))
             {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out hit))
+                //checks if you click on an enemy
+                if (Input.GetMouseButtonDown(0))
                 {
-                    if (hit.transform.tag == "Enemy")
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out hit))
                     {
-                    //records where the player is originally, to return to after attacking
-                        start_point = transform.position;
-                    //finds where the player will have to move to reach the enemy
-                        destination = hit.transform.position;
-                        if (destination.x > transform.position.x)
+                        if (hit.transform.tag == "Enemy")
                         {
-                        destination.x -= 1;
+                            attacking = true;
+                            //records where the player is originally, to return to after attacking
+                            start_point = transform.position;
+                            //finds where the player will have to move to reach the enemy
+                            destination = hit.transform.position;
+                            if (destination.x > transform.position.x)
+                            {
+                                destination.x -= 1;
+                            }
+                            else
+                            {
+                                destination.x += 1;
+                            }
+                            //sets the player as moving towards an enemy
+                            going = true;
                         }
-                        else
-                        {
-                        destination.x += 1;
-                        }
-                    //sets the player as moving towards an enemy
-                        going = true;
+
                     }
-
-                }
-            }
-
-            if (going)
-            {
-                //moves the player towards the selected enemy
-                transform.position = Vector3.MoveTowards(transform.position, destination, 0.2f);
-                //checks if the player has reached the enemy
-                if (transform.position == destination)
-                {
-                    //stops the player moving
-                    going = false;
-                    //starts the quicktime event
-                    RunQuickTime(attackSelected);
-                }
-            }
-            // QTResult will only be less than two once the quick time event has finished and returned the results
-            else if (rootUI.GetComponent<BattleMenuEvent>().QTResult < 2)
-            {
-                // Should be replaced by a function call
-                returning = true;
-            } 
-            if (returning)
-            {
-                //moves the player towards where it started
-                transform.position = Vector3.MoveTowards(transform.position, start_point, 0.2f);
-                //checks if the player has arrived back
-                if (transform.position == start_point)
-                {
-                    //stops the attack sequence, allowing another attack
-                    //Battle_UI.GetComponent<BattleMenuEvent>().attacking = false;
-                    //resets the attack sequence
-                    returning = false;
                 }
             }
         }
+
+                if (going)
+                {
+                    //moves the player towards the selected enemy
+                    transform.position = Vector3.MoveTowards(transform.position, destination, 0.2f);
+                    //checks if the player has reached the enemy
+                    if (transform.position == destination)
+                    {
+                        //stops the player moving
+                        going = false;
+                        //starts the quicktime event
+                        RunQuickTime(attackSelected);
+                    }
+                }
+                // QTResult will only be less than two once the quick time event has finished and returned the results
+                else if (rootUI.GetComponent<BattleMenuEvent>().QTResult < 2)
+                {
+                    // Should be replaced by a function call
+                    returning = true;
+                }
+                if (returning)
+                {
+                    //moves the player towards where it started
+                    transform.position = Vector3.MoveTowards(transform.position, start_point, 0.2f);
+                    //checks if the player has arrived back
+                    if (transform.position == start_point)
+                    {
+                        //stops the attack sequence, allowing another attack
+                        //Battle_UI.GetComponent<BattleMenuEvent>().attacking = false;
+                        //resets the attack sequence
+                        attacking = false;
+                        returning = false;
+                    }
+                }
+            }
+        
     }
-}
