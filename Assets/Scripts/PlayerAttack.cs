6eb8cc240f4982aba237959f0c364ad2435d1ab3
HDonovan96 <harry.bd11@gmail.com>
2019-11-21 12:06:40 +0000

    Fixed unity not recognising any scripts and began interfacing 'BattleMenuEvent' and 'PlayerAttack' script

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,46 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour
 {
     private Vector3 destination;
     private Vector3 start_point;
+    public string attackSelected = string.Empty;
     private bool going = false;
     public bool returning = false;
     private object selected_attack;
-    public GameObject Battle_UI;
+    private GameObject rootUI;
+    public Object areaPrefab;
+    QTFramework QTFramework;
+
+    public void RunQuickTime(string attack)
+    {
+        
+        // Initialises the sequence to pass to QTFramework.
+        // Is initialised as a new list to prevent crashing on unknown attack
+        List<KeyCode> quickTimeKeys = new List<KeyCode>();
+        // Instantiates the area that the quick time event will occur in as a child of the canvas.
+        GameObject QTArea = Instantiate(areaPrefab, rootUI.transform) as GameObject;
+        QTFramework = QTArea.GetComponentInChildren<QTFramework>();
+        // Checks which attack was selected and then sets the quick time button sequence accordingly.
+        // Currently uses placeholder sequences 
+            switch (attack)
+            {
+                case "Light":
+                {
+                    quickTimeKeys = new List<KeyCode>
+                    {
+                        KeyCode.L,
+                        KeyCode.I,
+                        KeyCode.G,
+                        KeyCode.H,
+                        KeyCode.T
+                    };
+                    break;
+                }
+                case "Medium":
+                {
+                    quickTimeKeys = new List<KeyCode>
+                    {
+                        KeyCode.M,
+                        KeyCode.E,
+                        KeyCode.D,
+                        KeyCode.I,
+                        KeyCode.U
+                    };
+                    break;
+                }
+                case "Heavy":
+                {
+                    quickTimeKeys = new List<KeyCode>
+                    {
+                        KeyCode.H,
+                        KeyCode.E,
+                        KeyCode.A,
+                        KeyCode.V,
+                        KeyCode.Y
+                    };
+                    break;
+                }
+                case "Sheep":
+                {
+                    quickTimeKeys = new List<KeyCode>
+                    {
+                        KeyCode.S,
+                        KeyCode.H,
+                        KeyCode.E,
+                        KeyCode.E,
+                        KeyCode.P
+                    };
+                    break;
+                }
+                default:
+                {
+                    Debug.Log("ERROR: Unkown attack");
+                    break;
+                }        
+            }
+            // Sends the selected sequence to the quick time area
+            QTFramework.sequence = quickTimeKeys;    
+    }
+
+    void Start()
+    {
+        rootUI = GameObject.FindWithTag("RootUI");
+    }
+
     void Update()
     {
         //checks if an attack button has been pressed
-        if (Battle_UI.GetComponent<BattleMenuEvent>().attacking)
+        if (!string.IsNullOrEmpty(attackSelected))
         {
             //checks if you click on an enemy
             if (Input.GetMouseButtonDown(0))
             {
-                    RaycastHit hit;
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                    if (Physics.Raycast(ray, out hit))
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.tag == "Enemy")
                     {
-                        if (hit.transform.tag == "Enemy")
+                    //records where the player is originally, to return to after attacking
+                        start_point = transform.position;
+                    //finds where the player will have to move to reach the enemy
+                        destination = hit.transform.position;
+                        if (destination.x > transform.position.x)
                         {
-                        //records where the player is originally, to return to after attacking
-                            start_point = transform.position;
-                        //finds where the player will have to move to reach the enemy
-                            destination = hit.transform.position;
-                            if (destination.x > transform.position.x)
-                            {
-                            destination.x -= 1;
-                            }
-                            else
-                            {
-                            destination.x += 1;
-                            }
-                        //sets the player as moving towards an enemy
-                            going = true;
+                        destination.x -= 1;
                         }
-
+                        else
+                        {
+                        destination.x += 1;
+                        }
+                    //sets the player as moving towards an enemy
+                        going = true;
                     }
+
+                }
             }
 
             if (going)
@@ -53,7 +134,7 @@ public class PlayerAttack : MonoBehaviour
                     //stops the player moving
                     going = false;
                     //starts the quicktime event
-                    Battle_UI.GetComponent<BattleMenuEvent>().Attacking(Battle_UI.GetComponent<BattleMenuEvent>().selected_button);
+                    RunQuickTime(attackSelected);
                 }
             }
             //if the quick time event has finished
@@ -65,7 +146,7 @@ public class PlayerAttack : MonoBehaviour
                 if (transform.position == start_point)
                 {
                     //stops the attack sequence, allowing another attack
-                    Battle_UI.GetComponent<BattleMenuEvent>().attacking = false;
+                    //Battle_UI.GetComponent<BattleMenuEvent>().attacking = false;
                     //resets the attack sequence
                     returning = false;
                 }
--- Assets/Scripts/UI scripts/Battle/BattleMenuEvent.cs
+++ Assets/Scripts/UI scripts/Battle/BattleMenuEvent.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 // Code is designed to sit in the 'Canvas - AttackScreen' in 'BattleScene' scene.
 public class BattleMenuEvent : MonoBehaviour
 {
-    public Object areaPrefab;
     public bool attacking = false;
-    public GameObject player;
-    public Button selected_button;
-    QTFramework QTFramework;
+    private GameObject player;
+    private PlayerAttack playerAttack;
+    public Button attackSelected;
     private bool quickTime = false;
     // Float to hold how well the player did at the quick time event
     // Initialises to two to mark that it hasn't been set by QTFramework. 
@@ -34,6 +33,10 @@ public class BattleMenuEvent : MonoBehaviour
         {
             selectedAttacks[i].text = gameController.selectedAttack[i];
         }
+
+        // Gets the player gameObject and PlayerAttack script for later use
+        player = GameObject.FindWithTag("Player");
+        playerAttack = player.GetComponent<PlayerAttack>();
     }
 
     void Start()
@@ -42,117 +45,50 @@ public class BattleMenuEvent : MonoBehaviour
     }
 
     //when an attack button is pressed
+    // public void OnAttackSelect(Button button)
+    // {
+    //     attackSelected = button;
+    //     //if the player isn't already in the attack sequence
+    //     if (!attacking)
+    //     {
+    //         //the player enters the attack sequence
+    //         attacking = true;
+    //     }
+    // }
+
     public void OnAttackSelect(Button button)
     {
-        selected_button = button;
-        //if the player isn't already in the attack sequence
-        if (!attacking)
-        {
-            //the player enters the attack sequence
-            attacking = true;
-        }
+        // Sets the selected attack in PlayerAttackScript to the button pressed
+        playerAttack.attackSelected = button.GetComponentInChildren<Text>().text;
     }
 
-    //This manages the quicktime events
-    public void Attacking(Button button)
-    {
-        // Initialises the sequence to pass to QTFramework.
-        // Is initialised as a new list to prevent crashing on unknown attack
-        List<KeyCode> quickTimeKeys = new List<KeyCode>();
-        // Instantiates the area that the quick time event will occur in as a child of the canvas.
-        GameObject QTArea = Instantiate(areaPrefab, transform) as GameObject;
-        QTFramework = QTArea.GetComponentInChildren<QTFramework>();
-        // Checks which attack was selected and then sets the quick time button sequence accordingly.
-        // Currently uses placeholder sequences
-        string selectedAttack = button.GetComponentInChildren<Text>().text;       
-            switch (selectedAttack)
-            {
-                case "Light":
-                {
-                    quickTimeKeys = new List<KeyCode>
-                    {
-                        KeyCode.L,
-                        KeyCode.I,
-                        KeyCode.G,
-                        KeyCode.H,
-                        KeyCode.T
-                    };
-                    break;
-                }
-                case "Medium":
-                {
-                    quickTimeKeys = new List<KeyCode>
-                    {
-                        KeyCode.M,
-                        KeyCode.E,
-                        KeyCode.D,
-                        KeyCode.I,
-                        KeyCode.U
-                    };
-                    break;
-                }
-                case "Heavy":
-                {
-                    quickTimeKeys = new List<KeyCode>
-                    {
-                        KeyCode.H,
-                        KeyCode.E,
-                        KeyCode.A,
-                        KeyCode.V,
-                        KeyCode.Y
-                    };
-                    break;
-                }
-                case "Sheep":
-                {
-                    quickTimeKeys = new List<KeyCode>
-                    {
-                        KeyCode.S,
-                        KeyCode.H,
-                        KeyCode.E,
-                        KeyCode.E,
-                        KeyCode.P
-                    };
-                    break;
-                }
-                default:
-                {
-                    Debug.Log("ERROR: Unkown attack");
-                    break;
-                }        
-            }
-            // Sends the selected sequence to the quick time area
-            QTFramework.sequence = quickTimeKeys;
-        //the quick time event has ended, tells the player to go back   
-    }
-    
-    private void Update()
-    {
-        if (attacking)
-        {
-            if (QTFramework.QTButtons.Count > 0)
-            {
-                quickTime = true;
-            }
-        }
-        if (quickTime)
-        {
-            if (QTFramework.QTButtons.Count == 0)
-            {
-                player.GetComponent<PlayerAttack>().returning = true;
-                quickTime = false;
-            }
-        }
+    // private void Update()
+    // {
+    //     if (attacking)
+    //     {
+    //         if (QTFramework.QTButtons.Count > 0)
+    //         {
+    //             quickTime = true;
+    //         }
+    //     }
+    //     if (quickTime)
+    //     {
+    //         if (QTFramework.QTButtons.Count == 0)
+    //         {
+    //             player.GetComponent<PlayerAttack>().returning = true;
+    //             quickTime = false;
+    //         }
+    //     }
 
-        // QTFramework will only ever return a value between 0 and 1.
-        // If a value has been returned by QTFramework prints QTResult.
-        // This action is placeholder.
-        // QTResult is then reset to two so this doesn't trigger continuously
-        if (QTResult <= 1)
-        {
-            Debug.Log(QTResult);
-            QTResult = 2;
-        }
-    }
+    //     // QTFramework will only ever return a value between 0 and 1.
+    //     // If a value has been returned by QTFramework prints QTResult.
+    //     // This action is placeholder.
+    //     // QTResult is then reset to two so this doesn't trigger continuously
+    //     if (QTResult <= 1)
+    //     {
+    //         Debug.Log(QTResult);
+    //         QTResult = 2;
+    //     }
+    // }
 
 }
