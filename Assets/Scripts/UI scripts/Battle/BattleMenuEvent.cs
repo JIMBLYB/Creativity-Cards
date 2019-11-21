3e8409491c4d726d5f41b4245a71dcd8887a41c5
HDonovan96 <harry.bd11@gmail.com>
2019-11-21 18:27:31 +0000

    In 'BattleMenuEvent' script removed unused 'using' statements

--- Assets/Scripts/UI scripts/Battle/BattleMenuEvent.cs
+++ Assets/Scripts/UI scripts/Battle/BattleMenuEvent.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 // Code is designed to sit in the 'Canvas - AttackScreen' in 'BattleScene' scene.
