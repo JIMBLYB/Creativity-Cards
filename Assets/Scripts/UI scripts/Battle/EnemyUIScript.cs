31ae7ae9018148d7669706d4e0c7ca5dba8a164e
NF230035 <NF230035@users.noreply.github.com>
2019-11-22 06:14:48 +0000

    Fixing some transitions between scenes (loading correct scenes) also implemented the enemy health script ready for the variable storing the enemy health.

--- Assets/Scripts/UI scripts/Battle/EnemyUIScript.cs
+++ Assets/Scripts/UI scripts/Battle/EnemyUIScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyHealthscript : MonoBehaviour
+public class EnemyUIScript : MonoBehaviour
 {
     [SerializeField]
     private float barFillAmount;
