8c11dec8d559a1b3e226a255dccdfd5a67b2c2f4
NF230035 <NF230035@users.noreply.github.com>
2019-11-22 05:41:24 +0000

    Player health now works and transitions should now work appropriately in win/lose conditions, but I cannot be certain because battle system seems to be broken.

---
+++ Assets/Scripts/UI scripts/Battle/HealthUIScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUIScript : MonoBehaviour
+{
+    [SerializeField]
+    private float barFillAmount;
+
+    [SerializeField]
+    private Image barFill;
+
+    public GameController GCInfo;
+
+
+    void Start()
+    {
+        GCInfo = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+
+    }
+
+    void Update()
+    {
+        //gets the player health and divides it by 100, so that its value can be transfered into the fillAmount
+        healthBar();
+        barFillAmount = GCInfo.health;
+        barFillAmount = barFillAmount / 100;
+    }
+
+    private void healthBar()
+    {
+        barFill.fillAmount = barFillAmount;
+    }
+}
